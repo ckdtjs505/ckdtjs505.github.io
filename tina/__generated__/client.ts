7cc10efc37237721e09a190668c7a61955fb57a5
sonia <sonia@soniaui-noteubug.local>
2026-09-09 12:28:04 +0900

    fix: tina update

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/sonia/Desktop/study/ckdtjs505.github.io/tina/__generated__/.cache/1788923628633', url: 'https://content.tinajs.io/1.6/content/1e9a3974-9235-4108-86ec-54dfa9bb832a/github/master', token: '6df9d96df969480c4050223475f4058415c36d93', queries,  });
+import { queries } from "./types.js";
+export const client = createClient({ cacheDir: '/Users/sonia/Desktop/study/ckdtjs505.github.io/tina/__generated__/.cache/1788924430003', url: 'https://content.tinajs.io/2.4/content/1e9a3974-9235-4108-86ec-54dfa9bb832a/github/master', token: '6df9d96df969480c4050223475f4058415c36d93', queries,  });
 export default client;
   
