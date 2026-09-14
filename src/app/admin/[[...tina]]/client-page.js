dea31a98c0ce1de117a33a49faee78a3d3e8bd49
sonia <sonia@soniaui-noteubug.local>
2026-09-14 09:37:15 +0900

    remove: tina

--- src/app/admin/[[...tina]]/client-page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TinaAdmin } from "tinacms";
-
-export default function ClientPage() {
-  return <TinaAdmin />;
-}
--- src/app/admin/[[...tina]]/page.js
+++
@@ -1,9 +0,0 @@
-import ClientPage from './client-page';
-
-export function generateStaticParams() {
-  return [{ tina: [] }];
-}
-
-export default function TinaAdminPage() {
-  return <ClientPage />;
-}
